85f86ceedce09909ab25ae0cb64a6e144d456ec9
Zeynab Manafova <zeynabmh1995@gmail.com>
2021-10-07 02:15:27 -0400

    cleared up

--- src/DisplayImages.js
+++ src/DisplayImages.js
@@ -2,20 +2,22 @@
 function DisplayImages( props ) {
 
     return (
-        <>
             <section className='bookContainer'>
-                <div className='images'>
-                    {props.books.map(getImages => {
-                        if (props.userBook === getImages.title) {
-                            return(
-                                <img src={getImages.book_image}  alt={getImages.description} /> 
-                            )
-                        }
-                    })}
-                </div>
+                <>
+                    <div className='images'>
+                        {props.books.map(getImages => {
+                            if (props.userBook === getImages.title) {
+                                return(
+                                    <img src={getImages.book_image}  alt={getImages.description} /> 
+                                )
+                            }
+                        })}
+                    </div>
+                </>
             </section>
-        </>
     );
-}
+};
 
 export default DisplayImages;
+
+
